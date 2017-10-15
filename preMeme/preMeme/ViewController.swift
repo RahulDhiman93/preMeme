0bdda6d4f9824c62acc706e073eb3ec784be9ae6
Rahul Dhiman <rahuldhiman@Rahuls-MacBook-Pro.local>
2017-10-15 22:26:45 +0530

    adding camera

---
+++ preMeme/preMeme/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  preMeme
+//
+//  Created by Rahul Dhiman on 15/10/17.
+//  Copyright © 2017 Rahul Dhiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate{
+
+   
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+    }
+    
+   
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+           imageView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func picker(_ sender: Any) {
+        
+        let pickController = UIImagePickerController()
+        pickController.delegate = self
+        pickController.sourceType = .photoLibrary
+        self.present(pickController, animated: true,completion: nil)
+        
+    }
+    
+    @IBAction func pickerCamera(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+        let pickController = UIImagePickerController()
+        pickController.delegate = self
+        pickController.sourceType = .camera
+        pickController.cameraCaptureMode = .photo
+        pickController.modalPresentationStyle = .fullScreen
+        
+        self.present(pickController, animated: true,completion: nil)
+    }
+        else{
+            let alertVC = UIAlertController(
+                title: "No Camera",
+                message: "Sorry, this device has no camera",
+                preferredStyle: .alert)
+        
+        present(
+            alertVC,
+            animated: true,
+            completion: nil)}
+    }
+    
+    
+
+}
+
